98b19d299cb0946dfff61f7a49ed126d1ad25267
Imran <imransanny619@gmail.com>
2022-12-09 20:56:10 +0600

    imran12/9/22

--- app/src/main/java/edu/ewubd/travelbd119/Home.java
+++ app/src/main/java/edu/ewubd/travelbd119/Home.java
@@ -1,14 +1,20 @@
 package edu.ewubd.travelbd119;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Home extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+
+
     }
+
+
 }
